113557a9b3f1ee29eacc319d63b81524795e93ab
lubahabarta <lubahabarta@gmail.com>
2022-09-13 15:26:29 +0200

    Footer.js a Footer.css pridan

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 //components
 import NavBar from './components/NavBar';
 import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
     return (
         <div className='app'>
             <Main />
+            <Footer />
         </div>
     );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <p>&copy; 2022 Takuya Matsuyama. All Rights Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
