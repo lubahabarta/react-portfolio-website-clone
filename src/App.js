908afb29723e8d4b3f6e5d262129619525535079
lubahabarta <lubahabarta@gmail.com>
2022-09-13 16:07:41 +0200

    <nav-bar> - hruby nastrel + css

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Footer from './components/Footer';
 function App() {
     return (
         <div className='app'>
+            <NavBar />
             <Main />
             <Footer />
         </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import './NavBar.css'
 
 function NavBar() {
-  return (
-    <div className='nav-bar'>NavBar</div>
-  )
+    return (
+        <div className='nav-bar'>
+            <div className='wrap'>
+                <div className="nav-bar-left">
+                    <h1>
+                        <svg>
+                            
+                        </svg>
+                        Takuya Matsuyama
+                    </h1>
+                </div>
+                <div className="nav-bar-center">
+                    <p>jfkdl;asjfk;ldas</p>
+                </div>
+                <div className="nav-bar-right">
+                    <p>fjkdl;asf;ldaks</p>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default NavBar
