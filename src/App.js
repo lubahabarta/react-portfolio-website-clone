f679d91a375b4b286dddc32d5f03945d015bf2ab
lubahabarta <lubahabarta@gmail.com>
2022-09-17 20:51:40 +0200

    odebral jsem vantu a pridal vlastni obrazek v Main.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import './App.css';
 //components
 import NavBar from './components/NavBar';
 import Main from './components/Main';
-import Footer from './components/Footer';
 
 function App() {
     return (
         <div className='app'>
             <NavBar />
             <Main />
-            <Footer />
         </div>
     );
 }
